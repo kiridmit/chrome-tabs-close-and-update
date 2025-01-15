876a1697a471ae27b5c6e0c697bc1249ed8dd57c
dkirienko-umg <dkirienko@umg-marketing.com>
2025-01-16 00:18:23 +0100

    New functions - close, create, restore and update

--- background.js
+++ background.js
@@ -1,21 +1,37 @@
 chrome.commands.onCommand.addListener(function (command) {
-    if (command === "reload-tab") {
-        reloadTab();
-    } else if (command === "close-tab") {
-        closeTab();
-    }
+    chrome.tabs.query({ currentWindow: true, active: true }, function (tabs) {
+        if (!tabs.length) return;
+        const currentTab = tabs[0];
+
+        switch (command) {
+            case "reload-tab":
+                reloadTab(currentTab);
+                break;
+            case "close-tab":
+                closeTab(currentTab);
+                break;
+            case "create-tab":
+                createTab(currentTab);
+                break;
+            case "restore-tab":
+                restoreTab();
+                break;
+        }
+    });
 });
 
-function reloadTab() {
-    chrome.tabs.query({ currentWindow: true, active: true }, function (tabs) {
-        let tab = tabs[0];
-        chrome.tabs.reload(tab.id);
-    });
+function reloadTab(currentTab) {
+    chrome.tabs.reload(currentTab.id);
 }
 
-function closeTab() {
-    chrome.tabs.query({ currentWindow: true, active: true }, function (tabs) {
-        let tab = tabs[0];
-        chrome.tabs.remove(tab.id);
-    });
+function closeTab(currentTab) {
+    chrome.tabs.remove(currentTab.id);
+}
+
+function createTab(currentTab) {
+    chrome.tabs.create({ index: currentTab.index + 1, active: true });
+}
+
+function restoreTab() {
+    chrome.sessions.restore();
 }
